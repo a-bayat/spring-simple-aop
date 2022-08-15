4a11f33205a5e7aa8eaa4533632fd523f278d677
Bayat <A.Bayat@shaparak.com>
2022-08-15 16:12:46 +0430

    AOP AfterFinally

---
+++ src/main/java/com/luv2code/aopdemo/AfterFinallyDemoApp.java
@@ -0,0 +1,34 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class AfterFinallyDemoApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        // call method to find the accounts
+        List<Account> accounts = null;
+
+        try {
+            // add a boolean flag to simulate exception
+            boolean tripWire = false;
+            accounts = accountDAO.findAccounts(tripWire);
+        } catch (Exception ex) {
+            System.out.println("\n\nMain Program ... caught exception: " + ex);
+        }
+
+        System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+        System.out.println("-----");
+
+        System.out.println(accounts);
+
+        System.out.println("\n");
+
+        context.close();
+    }
+}
--- src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,10 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -18,7 +15,13 @@ import java.util.List;
 @Order(2)
 public class MyDemoLoggingAspect {
 
-    // add new advice for @AfterReturning on the findAccounts method
+
+    @After("execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))")
+    public void afterFinallyFindAccountsAdvice(JoinPoint joinPoint) {
+        // print out which method we are advising on
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("\n===========>>> Executing @After (finally) on method: " + method);
+    }
 
     @AfterThrowing(
             pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
